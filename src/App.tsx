2d8a38c9466cd95b1008d13faa27f53c469d23f4
Suchitra Swain <suchitraswain.2012gmail.com>
2023-10-19 00:44:39 +0200

    home section

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutUs from "./pages/AboutUs/AboutUs";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Footer from "./components/Footer";
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import profile from "../../assets/profile.png";
+
+const Home = () => {
+  return (
+    <section className="home__section mt-5 pt-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <img
+              src={profile}
+              alt="sushmita"
+              className="img-fluid d-block mx-auto"
+              style={{ width: "140px" }}
+            />
+          </div>
+          <div className="col-md-12 mt-5">
+            <h1 className="fw-bold text-center text-light">
+              Bonjour,{" "}
+              <span className="text__gradient">I&apos;m Sushmita Swain</span>
+            </h1>
+            <h2 className="text-center text-light">
+              a Creative and Product Designer based in India
+            </h2>
+          </div>
+          <div className="col-md-12 mt-5">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="text-center mt-3 text-light">
+                  I specialize in designing immersive digital products, where
+                  storytelling and prototyping are my forte.
+                </h5>
+                <p className="text-center mt-3 text__pink">
+                  “Crafting solutions that bridge the gap between technologies &
+                  everyday life”
+                </p>
+                <p className="text-center text-light">
+                  Product Design | Design System | Research | Micro-Interaction
+                  | Illustrations
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
